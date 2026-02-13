e83240ca9715d16f8c5bc4c4e1bc2f9085767657
Jare7028 <jared@resolvable.com>
2026-02-13 09:15:51 +0000

    Fix payroll teardown dependency order

--- sql/employee_payroll.sql
+++ sql/employee_payroll.sql
@@ -17,12 +17,12 @@ begin
 end
 $$;
 
-drop function if exists public.can_edit_employee_payroll_row(uuid);
-drop function if exists public.can_view_employee_payroll_row(uuid);
-drop function if exists public.can_access_employee_payroll();
-
 drop table if exists public.employee_payroll_cell_values cascade;
 drop table if exists public.employee_payroll_row_users cascade;
 drop table if exists public.employee_payroll_columns cascade;
 drop table if exists public.employee_payroll_dropdown_options cascade;
 drop table if exists public.employee_payroll_rows cascade;
+
+drop function if exists public.can_edit_employee_payroll_row(uuid) cascade;
+drop function if exists public.can_view_employee_payroll_row(uuid) cascade;
+drop function if exists public.can_access_employee_payroll() cascade;
